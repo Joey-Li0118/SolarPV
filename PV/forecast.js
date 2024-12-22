4d3e6bdf6e689146a091a21cf1e28fd2bf674562
JustJokingAround <imageheart89@gmail.com>
2024-12-22 00:17:51 -0600

    adding 404 error and styling css

---
+++ PV/forecast.js
@@ -0,0 +1,5 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.search');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.conditions');
+const error404 = document.querySelector('.notfound');
